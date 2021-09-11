488c76819108c33b3b3e08fdf909e637b8cb7d33
dimasrifalta <dimasrifalta@gmail.com>
2021-09-11 16:37:11 +0700

    Add new app project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,13 +10,13 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'E-Commerce',
+    title: 'Bucek Official Store',
     description:
       "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
     image: '/images/2.png',
     tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://bucek-officcial.netlify.app/',
+    visit: 'https://bucek-officcial.netlify.app/',
     id: 1,
   },
   {
